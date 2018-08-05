83520d72c9e1ef300ec6b60ecaf9cf1672e96e3e
Peter Wilson <git-commit-by@peterwilson.cc>
2018-08-05 12:48:24 +0100

    Check if Jetpack.css is really needed.
    This runs through all the Jetpack registered styles and checks how many are enqueued before enqueuing the concatenated file.
    
    If two or more are enqueued then the concatenated file is loaded. In the days of H2 this could be smarter but eh?
    
    If zero or one file are enqueued then the concatenated file is not loaded because the original approach will result in a smaller transfer.

--- inc/jetpack-fixes/namespace.php
+++ inc/jetpack-fixes/namespace.php
@@ -17,4 +17,37 @@ use Jetpack;
  * Runs on the `plugins_loaded` hook.
  */
 function bootstrap() {
+	add_filter( 'jetpack_implode_frontend_css', __NAMESPACE__ . '\\maybe_implode_css' );
+}
+
+/**
+ * Use a bit of smarts to determine if Jetpack CSS should be imploded.
+ *
+ * Runs on the filter `jetpack_implode_frontend_css`.
+ *
+ * @param bool $do_implode Initial decision to implode/not implode CSS.
+ * @return bool Whether to implode CSS.
+ */
+function maybe_implode_css( bool $do_implode ) {
+	if ( ! $do_implode ) {
+		// It's already been decided not to implode.
+		return $do_implode;
+	}
+
+	$jetpack = Jetpack::init();
+	$jetpack_styles = $jetpack->concatenated_style_handles;
+	$enqueued_count = 0;
+
+	foreach ( $jetpack_styles as $style ) {
+		if ( wp_style_is( $style, 'enqueued' ) ) {
+			$enqueued_count++;
+		}
+
+		if ( $enqueued_count >= 2 ) {
+			// Two or more files enqueued, no further checks required.
+			break;
+		}
+	}
+
+	return $enqueued_count >= 2 ? $do_implode : false;
 }
