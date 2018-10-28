6090cbd71cb4f2b5327077ea82b1a1ecd477cbaa
Peter Wilson <git-commit-by@peterwilson.cc>
2018-10-28 14:05:37 +1000

    Revert "Only include image_meta in max width if we know it."
    This reverts commit 2075009f7d747fb7987b7760935e66799ffbeaae.

--- inc/tachyon-mods/namespace.php
+++ inc/tachyon-mods/namespace.php
@@ -410,11 +410,7 @@ function add_srcset_and_sizes( $image_data, $image_meta, $attachment_id ) {
 	 * - full size image
 	 */
 	$min_width = 480;
-	$max_widths = [ $content_width * 2, $width * 2 ];
-	if ( isset( $image_meta['width'] ) ) {
-		$max_widths[] = $image_meta['width'];
-	}
-	$max_width = min( $max_widths );
+	$max_width = min( $content_width * 2, $width * 2, $image_meta['width'] );
 	$candidates = 5;
 
 	if ( $max_width <= $min_width ) {
