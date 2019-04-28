247d5aef93aa54f03ec096edc667b3fb48f74b77
Peter Wilson <git-commit-by@peterwilson.cc>
2019-04-28 15:55:38 +1000

    Allow for smaller image minimum widths.

--- inc/tachyon-mods/namespace.php
+++ inc/tachyon-mods/namespace.php
@@ -483,8 +483,8 @@ function add_srcset_and_sizes( $image_data, $image_meta, $attachment_id ) {
 	 * - display size * 2
 	 * - full size image
 	 */
-	$min_width = 480;
-	$max_width = min( $content_width * 2, $width * 2, $image_meta['width'] );
+	$min_width = min( $width, 480 );
+	$max_width = min( $content_width * 2, $width * 2, $crop_width );
 	$candidates = 5;
 
 	if ( $max_width <= $min_width ) {
